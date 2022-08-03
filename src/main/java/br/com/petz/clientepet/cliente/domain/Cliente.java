7492be9abba924a3afe617fc54373d36bd0d726a
Felipendev <felipe.psic@outlook.com.br>
2022-08-03 10:58:08 -0300

    refatora pacote das entidades e ajusta mapeamento id uuid

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -1,12 +1,11 @@
-package br.com.petz.clientepet.cliente;
+package br.com.petz.clientepet.cliente.domain;
 
-import br.com.petz.clientepet.cliente.sexo.Sexo;
 import lombok.AccessLevel;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -15,9 +14,12 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
     private UUID idCliente;
     @NotBlank
     private String nomeCompleto;
@@ -43,7 +45,6 @@ public class Cliente {
                    String telefone, Sexo sexo,
                    LocalDate dataNascimento, String cpf,
                    Boolean aceitaTermos) {
-        this.idCliente = UUID.randomUUID();
         this.nomeCompleto = nomeCompleto;
         this.email = email;
         this.celular = celular;
--- src/main/java/br/com/petz/clientepet/cliente/sexo/Sexo.java
+++
@@ -1,5 +0,0 @@
-package br.com.petz.clientepet.cliente.sexo;
-
-public enum Sexo {
-    FEMININO,MASCULINO;
-}
