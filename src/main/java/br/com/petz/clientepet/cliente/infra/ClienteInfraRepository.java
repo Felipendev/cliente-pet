23393fcd09b3de17165c2f2e34666899e47f570f
Felipendev <felipe.psic@outlook.com.br>
2022-08-04 10:22:26 -0300

    implementa cliente repository, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -2,15 +2,24 @@ package br.com.petz.clientepet.cliente.infra;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+import java.util.Properties;
+
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
+
+
+    private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
+
     @Override
     public Cliente salva(Cliente cliente) {
        log.info("[start] ClienteInfraRepository - salva");
+       clienteSpringDataJPARepository.save(cliente);
        log.info("[finish] ClienteInfraRepository - salva");
         return cliente;
     }
