150dab37b34a0205a050b4b25b06b982211dde25
Felipendev <felipe.psic@outlook.com.br>
2022-08-04 08:57:13 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,29 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
 }
