762f7171e5830761aa75110f4e8d384b2fd2bc8e
Felipendev <felipe.psic@outlook.com.br>
2022-08-04 10:41:28 -0300

    implementa fluxo api, test ok

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Log4j2
 @RequiredArgsConstructor
@@ -17,5 +19,12 @@ public class ClienteController implements ClienteApi {
         ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
         log.info("[finish] ClienteController - postCliente");
         return clienteCriado;
+    }
+
+    @Override
+    public List<ClienteListResponse> getTodosClientes() {
+        log.info("[start] ClienteController - getTodosClientes");
+        log.info("[finish] ClienteController - getTodosClientes");
+        return null;
     }
 }
