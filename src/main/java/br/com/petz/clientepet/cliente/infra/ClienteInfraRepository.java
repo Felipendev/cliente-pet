210c0d09041fb0efc10c6f7884e0e6ca9737899e
Felipendev <felipe.psic@outlook.com.br>
2022-08-04 20:51:53 -0300

    implementa handler

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -2,8 +2,10 @@ package br.com.petz.clientepet.cliente.infra;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import br.com.petz.clientepet.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -36,7 +38,7 @@ public class ClienteInfraRepository implements ClienteRepository {
     public Cliente buscaClienteAtravesId(UUID idCliente) {
         log.info("[start] ClienteInfraRepository - buscaClienteAtravesId");
         Cliente cliente = clienteSpringDataJPARepository.findById(idCliente)
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado! "));
+                .orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
         log.info("[finish] ClienteInfraRepository - buscaClienteAtravesId");
         return cliente;
     }
