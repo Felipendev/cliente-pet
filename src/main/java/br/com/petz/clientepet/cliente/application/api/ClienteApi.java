a68afd7363faddc68f22abebd43d099a3b6e34fd
Felipendev <felipe.psic@outlook.com.br>
2022-08-04 09:19:53 -0300

    implementa teste do service

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteApi.java
@@ -3,10 +3,12 @@ package br.com.petz.clientepet.cliente.application.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteApi {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
+    ClienteResponse postCliente(@RequestBody @Valid ClienteRequest clienteRequest);
 }
